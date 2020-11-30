c8447a689cf6c37a096d03eda42cacc4517b273c
Wesley Gilbert <itswesleyg@gmail.com>
2020-11-30 18:31:23 -0500

    Jumping and jumping animations are in, just need to get them attached to the animator object

--- NecroCyberNecromancer/Assets/Scripts/PlayerController.cs
+++ NecroCyberNecromancer/Assets/Scripts/PlayerController.cs
@@ -11,29 +11,62 @@ public class PlayerController : MonoBehaviour
     Vector3 forwardMovement;
     Vector3 rightMovement;
     private Animator anim;
+    private Rigidbody rb;
+
+    //Jumping vars
+    [SerializeField] private float checkRadius=0.1f;
+    [SerializeField] private float jumpForce=10;
+    Vector3 movement;
+    [SerializeField] private float rayCastJumpDistance = 1f;
     // Start is called before the first frame update
     void Start()
     {
         charController = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
-
-     
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();
+        
+       // Movement();
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        float hAxis = Input.GetAxisRaw("Horizontal");
+        float vAxis = Input.GetAxisRaw("Vertical");
 
+        Vector3 movement = new Vector3(hAxis, 0, vAxis)*movementSpeed;
+
+        Vector3 newPosition = rb.position + rb.transform.TransformDirection(movement);
+
+        rb.MovePosition(newPosition);
+        forwardMovement = transform.forward * vAxis;
+        rightMovement = transform.right * hAxis;
+        anim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("speed", (forwardMovement.sqrMagnitude + rightMovement.sqrMagnitude));
+    }
     private void Movement()
     {
-        vertInput = Input.GetAxis("Vertical") * movementSpeed; 
-        horizInput = Input.GetAxis("Horizontal")*movementSpeed;
+        vertInput = Input.GetAxis("Vertical"); 
+        horizInput = Input.GetAxis("Horizontal");
+        
+        movement = new Vector3(horizInput, 0, vertInput);
+        rb.AddForce(movement);
         forwardMovement = transform.forward * vertInput;
         rightMovement = transform.right * horizInput;
-        charController.SimpleMove(forwardMovement + rightMovement);
-
+       // charController.SimpleMove(forwardMovement + rightMovement);
+        
 
         // anim.SetFloat("horizontal", (forwardMovement.x+rightMovement.x));
         // anim.SetFloat("vertical", (forwardMovement.z + rightMovement.z));
@@ -41,5 +74,15 @@ public class PlayerController : MonoBehaviour
         anim.SetFloat("vertical", Input.GetAxis("Vertical"));
         anim.SetFloat("speed", (forwardMovement.sqrMagnitude + rightMovement.sqrMagnitude));
 
+       
+
     }
+    private bool IsGrounded()
+    {
+        Debug.DrawRay(transform.position, Vector3.down*rayCastJumpDistance, Color.blue);
+      return  (Physics.Raycast(transform.position, Vector3.down, rayCastJumpDistance));
+        
+    }
+     
+
 }
