b6283034bf4dec898d8c79183494810a94060973
Terran <terranorion@gmail.com>
2020-12-12 21:24:39 -0500

    Added hazards and more enemies and completed level areas

---
+++ NecroCyberNecromancer/Assets/Scripts/Terran/Checkpoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField]
+    FallZone fallZoneRef = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            fallZoneRef.recoveryCheckpoint = this.transform;
+        }
+    }
+}
---
+++ NecroCyberNecromancer/Assets/Scripts/Terran/FallZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallZone : MonoBehaviour
+{
+    public Transform recoveryCheckpoint = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            other.gameObject.GetComponent<PlayerStats>().PlayerTakeDamage(5);
+            other.gameObject.transform.position = recoveryCheckpoint.position;
+        }
+    }
+}
